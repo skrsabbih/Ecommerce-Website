c80912ae0515a76b3eeee2da0cfe22c61bfd1fd5
mahbub14 <mahbubhasan14@gmail.com>
2025-01-25 22:31:05 +0600

    Create dashboards for the users

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
