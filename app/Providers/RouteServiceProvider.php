de60c2a9f5c793c6237b72e53296312f84f12d8d
mahbub14 <mahbubhasan14@gmail.com>
2025-01-25 23:58:37 +0600

    Updated RouteServiceProvider for multi-auth

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -40,6 +40,8 @@ class RouteServiceProvider extends ServiceProvider
                 ->as('admin')
                 ->group(base_path('routes/admin.php'));
             Route::middleware(['web','auth','role:vendor'])
+                ->prefix('vendor')
+                ->as('vendor')
                 ->group(base_path('routes/vendor.php'));
         });
     }
