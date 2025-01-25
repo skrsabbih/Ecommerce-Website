181b9beb0430e05639eeb54be72dc37b32540271
mahbub14 <mahbubhasan14@gmail.com>
2025-01-25 21:06:48 +0600

    Ecommerce Multi Auth Setup With Breeze

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::insert([
+            [
+                'name' => 'Admin User',
+                'username' => 'adminuser',
+                'email' => 'admin@gmail.com',
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'Vendor User',
+                'username' => 'vendoruser',
+                'email' => 'vendor@gmail.com',
+                'role' => 'vendor',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'role' => 'user',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ]
+        ]);
+    }
+}
