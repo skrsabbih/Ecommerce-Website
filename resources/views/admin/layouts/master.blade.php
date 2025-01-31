8081dff4752d39f6a440729e621be74d0bb957d5
Md. Sabbih Sarker <Sabbihkyau@gmail.com>
2025-01-31 20:33:10 +0600

    Add Toastr for admin panel

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
@@ -14,6 +15,7 @@
     <link rel="stylesheet" href="{{asset('backend/assets/modules/weather-icon/css/weather-icons.min.css')}}">
     <link rel="stylesheet" href="{{asset('backend/assets/modules/weather-icon/css/weather-icons-wind.min.css')}}">
     <link rel="stylesheet" href="{{asset('backend/assets/modules/summernote/summernote-bs4.css')}}}">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
     <!-- Template CSS -->
     <link rel="stylesheet" href="{{asset('backend/assets/css/style.css')}}">
@@ -22,61 +24,72 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag() { dataLayer.push(arguments); }
         gtag('js', new Date());
 
         gtag('config', 'UA-94034622-3');
     </script>
-    <!-- /END GA --></head>
+    <!-- /END GA -->
+</head>
 
 <body>
-<div id="app">
-    <div class="main-wrapper main-wrapper-1">
-        <div class="navbar-bg"></div>
-        <!-- Navbar Content -->
-        @include('admin.layouts.navbar')
-        <!-- Navbar Content End -->
-        <!-- Sidebar Content -->
-        @include('admin.layouts.sidebar')
-        <!-- Sidebar Content End -->
-        <!-- Main Content -->
-        <div class="main-content">
-            @yield('content')
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <div class="navbar-bg"></div>
+            <!-- Navbar Content -->
+            @include('admin.layouts.navbar')
+            <!-- Navbar Content End -->
+            <!-- Sidebar Content -->
+            @include('admin.layouts.sidebar')
+            <!-- Sidebar Content End -->
+            <!-- Main Content -->
+            <div class="main-content">
+                @yield('content')
+            </div>
+            <footer class="main-footer">
+                <div class="footer-left">
+                    Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad
+                        Nauval Azhar</a>
+                </div>
+                <div class="footer-right">
+
+                </div>
+            </footer>
         </div>
-        <footer class="main-footer">
-            <div class="footer-left">
-                Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
-            </div>
-            <div class="footer-right">
-
-            </div>
-        </footer>
     </div>
-</div>
 
-<!-- General JS Scripts -->
-<script src="{{asset('backend/assets/modules/jquery.min.js')}}"></script>
-<script src="{{asset('backend/assets/modules/popper.js')}}"></script>
-<script src="{{asset('backend/assets/modules/tooltip.js')}}"></script>
-<script src="{{asset('backend/assets/modules/bootstrap/js/bootstrap.min.js')}}"></script>
-<script src="{{asset('backend/assets/modules/nicescroll/jquery.nicescroll.min.js')}}"></script>
-<script src="{{asset('backend/assets/modules/moment.min.js')}}"></script>
-<script src="{{asset('backend/assets/js/stisla.js')}}"></script>
+    <!-- General JS Scripts -->
+    <script src="{{asset('backend/assets/modules/jquery.min.js')}}"></script>
+    <script src="{{asset('backend/assets/modules/popper.js')}}"></script>
+    <script src="{{asset('backend/assets/modules/tooltip.js')}}"></script>
+    <script src="{{asset('backend/assets/modules/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('backend/assets/modules/nicescroll/jquery.nicescroll.min.js')}}"></script>
+    <script src="{{asset('backend/assets/modules/moment.min.js')}}"></script>
+    <script src="{{asset('backend/assets/js/stisla.js')}}"></script>
 
-<!-- JS Libraies -->
-<script src="{{asset('backend/assets/modules/simple-weather/jquery.simpleWeather.min.js')}}"></script>
-<script src="{{asset('backend/assets/modules/chart.min.js')}}"></script>
-<script src="{{asset('backend/assets/modules/jqvmap/dist/jquery.vmap.min.js')}}"></script>
-<script src="{{asset('backend/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
-<script src="{{asset('backend/assets/modules/summernote/summernote-bs4.js')}}"></script>
-<script src="{{asset('backend/assets/modules/chocolat/dist/js/jquery.chocolat.min.js')}}"></script>
+    <!-- JS Libraies -->
+    <script src="{{asset('backend/assets/modules/simple-weather/jquery.simpleWeather.min.js')}}"></script>
+    <script src="{{asset('backend/assets/modules/chart.min.js')}}"></script>
+    <script src="{{asset('backend/assets/modules/jqvmap/dist/jquery.vmap.min.js')}}"></script>
+    <script src="{{asset('backend/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
+    <script src="{{asset('backend/assets/modules/summernote/summernote-bs4.js')}}"></script>
+    <script src="{{asset('backend/assets/modules/chocolat/dist/js/jquery.chocolat.min.js')}}"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
-<!-- Page Specific JS File -->
-<script src="{{asset('backend/assets/js/page/index-0.js')}}"></script>
+    <!-- Page Specific JS File -->
+    <script src="{{asset('backend/assets/js/page/index-0.js')}}"></script>
 
-<!-- Template JS File -->
-<script src="{{asset('backend/assets/js/scripts.js')}}"></script>
-<script src="{{asset('backend/assets/js/custom.js')}}"></script>
+    <!-- Template JS File -->
+    <script src="{{asset('backend/assets/js/scripts.js')}}"></script>
+    <script src="{{asset('backend/assets/js/custom.js')}}"></script>
+    <script>
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+               toastr.error("{{$error}}")
+            @endforeach
+        @endif
+    </script>
 
 </body>
+
 </html>
--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -33,11 +33,11 @@
                     </div>                            
                     <div class="form-group col-md-6 col-12">
                       <label>Name</label>
-                      <input type="text" name="name" class="form-control" value="{{Auth::user()->name}}" required="">
+                      <input type="text" name="name" class="form-control" value="{{Auth::user()->name}}" >
                     </div>
                     <div class="form-group col-md-6 col-12">
                       <label>Email</label>
-                      <input type="text" name="email" class="form-control" value="{{Auth::user()->email}}" required="">
+                      <input type="text" name="email" class="form-control" value="{{Auth::user()->email}}">
                     </div>
                   </div>
                  
@@ -50,11 +50,7 @@
         </div>
         <div class="col-12 col-md-12 col-lg-7">
           <div class="card">
-            @if ($errors->any())
-                @foreach ($errors->all() as $error)
-                    <span class="alert alert-danger">{{$error}}</span>
-                @endforeach
-            @endif
+           
             <form method="post" class="needs-validation" novalidate="" action="{{route('admin.password.update')}}"
             enctype="multipart/form-data">
               @csrf
