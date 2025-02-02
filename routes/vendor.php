25d881ab5138ab094421093b94789b7909597413
mahbub14 <mahbubhasan14@gmail.com>
2025-02-02 23:18:10 +0600

    Update vendor profile functionality and details

--- routes/vendor.php
+++ routes/vendor.php
@@ -1,5 +1,9 @@
 <?php
 use App\Http\Controllers\Backend\VendorController;
+use App\Http\Controllers\Backend\vendorProfileController;
 use Illuminate\Support\Facades\Route;
 /** Vendor Routes **/
 Route::get('dashboard',[VendorController::class,'dashboard'])->name('dashboard');
+Route::get('profile',[vendorProfileController::class,'index'])->name('profile');
+Route::put('profile', [vendorProfileController::class, 'updateProfile'])->name('profile.update');//vendor.profile.update
+Route::post('profile', [vendorProfileController::class, 'updatePassword'])->name('profile.update.password');//vendor.profile.update.password
