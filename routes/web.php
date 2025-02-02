f6cf72bbf91563e4c86cd4f370a642a740c2fa3c
mahbub14 <mahbubhasan14@gmail.com>
2025-02-02 09:46:09 +0600

    add user profile update section

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\UserDashboardController;
+use App\Http\Controllers\Frontend\UserProfileController ;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Backend\AdminController;
 use Illuminate\Support\Facades\Route;
@@ -34,4 +35,5 @@ Route::group([
     'as' => 'user.'
 ], function () {
     Route::get('dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+    Route::get('profile', [UserProfileController::class, 'index'])->name('profile');
 });
