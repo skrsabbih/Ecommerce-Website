b6bc4ecaebf934049e55840a4b6b58a381d49d11
mahbub14 <mahbubhasan14@gmail.com>
2025-02-04 13:46:22 +0600

    implemnt update functionality to Slider data

--- app/Traits/imageUploadTrait.php
+++ app/Traits/imageUploadTrait.php
@@ -1,19 +1,44 @@
 <?php
+
 namespace App\Traits;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
-trait ImageUploadTrait{
-
-    public function uploadImage(Request $request,$inputName,$path){
-        if($request->hasFile($inputName))
-        {            
+trait ImageUploadTrait
+{
+    public function uploadImage(Request $request, $inputName, $path)
+    {
+        if ($request->hasFile($inputName)) {
             $image = $request->{$inputName};
-            $ext=$image->getClientOriginalExtension();
-            $imageName = 'media_'.uniqid().'.'.$ext;
+            $ext = $image->getClientOriginalExtension();
+            $imageName = 'media_' . uniqid() . '.' . $ext;
             $image->move(public_path($path), $imageName);
-        
 
-            return $path.'/'.$imageName;
+            return $path . '/' . $imageName;
         }
+        return null; // Return null if no file was uploaded
+    }
+
+    public function updateImage(Request $request, $inputName, $path, $oldPath = null)
+    {
+        if ($request->hasFile($inputName)) {
+            // Delete the old file if it exists
+            if ($oldPath && File::exists(public_path($oldPath))) {
+                File::delete(public_path($oldPath));
+            }
+
+            // Handle the new image upload
+            $image = $request->{$inputName};
+            $ext = $image->getClientOriginalExtension();
+            $imageName = 'media_' . uniqid() . '.' . $ext;
+            $image->move(public_path($path), $imageName);
+
+            // Return the new image path
+            return $path . '/' . $imageName;
+        }
+
+        // If no new image is uploaded, return the old image path (if provided)
+        return $oldPath;
     }
 }
