05b7c79132a0fbb5874eb884ce567403c0152a5e
mahbub14 <mahbubhasan14@gmail.com>
2025-02-11 23:00:40 +0600

    fix slider datatable issue

--- app/DataTables/SliderDataTable.php
+++ app/DataTables/SliderDataTable.php
@@ -22,24 +22,25 @@ class SliderDataTable extends DataTable
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-            ->addColumn('action', function($query) {
-                $editBtn = "<a href='".route('admin.slider.edit', $query->id)."' class='btn btn-primary'><i class='far fa-edit'></i></a>";
-                $deleteBtn = "<a href='".route('admin.slider.destroy', $query->id)."' class='btn btn-danger delete-item'><i class='fas fa-trash'></i></a>";
-                
+            ->addColumn('action', function($query){
+               $editBtn = "<a href='".route('admin.slider.edit', $query->id)."' class='btn btn-primary'><i class='far fa-edit'></i></a>";
+               $deleteBtn = "<a href='".route('admin.slider.destroy', $query->id)."' class='btn btn-danger ml-2 delete-item'><i class='fas fa-trash'></i></a>";
 
-                return "<div style='display: flex; gap: 5px;'>" . $editBtn . $deleteBtn . "</div>";
+               return $editBtn.$deleteBtn;
             })
-            
-            ->addColumn('banner', function($query) {
-                return $img = "<img width='200 px' src=".asset($query->banner)." ></img>"; 
+            ->addColumn('banner', function($query){
+              return $img = "<img width='100px' src='".asset($query->banner)."' ></img>";
             })
-            ->addColumn('status', function ($query) {
-                return $query->status == 1 
-                    ? '<i class="badge badge-success">Active</i>' 
-                    : '<i class="badge badge-danger">Inactive</i>';
+            ->addColumn('status', function($query){
+                $active = '<i class="badge badge-success">Active</i>';
+                $inActive = '<i class="badge badge-danger">Inactive</i>';
+                if($query->status == 1){
+                    return $active;
+                }else {
+                    return $inActive;
+                }
             })
-            
-            ->rawColumns(['banner','action','status'])
+            ->rawColumns(['banner', 'action', 'status'])
             ->setRowId('id');
     }
 
@@ -79,18 +80,17 @@ class SliderDataTable extends DataTable
     public function getColumns(): array
     {
         return [
-            
-            Column::make('id'),
-            Column::make('banner'),
+
+            Column::make('id')->width(100),
+            Column::make('banner')->width(200),
             Column::make('title'),
             Column::make('serial'),
             Column::make('status'),
             Column::computed('action')
                   ->exportable(false)
                   ->printable(false)
-                  ->width(60)
+                  ->width(200)
                   ->addClass('text-center'),
-           
         ];
     }
 
