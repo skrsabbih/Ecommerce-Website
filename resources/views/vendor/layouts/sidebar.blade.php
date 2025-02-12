70e0636250ffc1548a3b11947859b4d1e25b4b12
mahbub14 <mahbubhasan14@gmail.com>
2025-02-12 15:35:25 +0600

    creating necessary files for vendor

--- resources/views/vendor/layouts/sidebar.blade.php
+++ resources/views/vendor/layouts/sidebar.blade.php
@@ -12,6 +12,8 @@
               <i class="fas fa-tachometer"></i> Dashboard
           </a>
       </li>
+      <li><a class="{{setActive(['vendor.products.*'])}}" href="{{route('vendor.products.index')}}"><i class="fas fa-cart-plus"></i> Products</a></li>
+
       <li><a href="{{route('vendor.shop-profile.index')}}"><i class="far fa-user"></i> Shop Profile</a></li>
       <li><a href="{{route('vendor.profile')}}"><i class="far fa-user"></i> My Profile</a></li>
       
--- routes/vendor.php
+++ routes/vendor.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Backend\vendorProfileController;
 use App\Http\Controllers\Backend\VendorShopProfileController;
+use App\Http\Controllers\Backend\VendorProductController;
 use Illuminate\Support\Facades\Route;
 /** Vendor Routes **/
 Route::get('dashboard',[VendorController::class,'dashboard'])->name('dashboard');
@@ -11,3 +12,8 @@ Route::post('profile', [vendorProfileController::class, 'updatePassword'])->name
 
 /** Vendor shop profile  */
 Route::resource('shop-profile', VendorShopProfileController::class);
+/** Product Routes */
+Route::get('product/get-subcategories', [VendorProductController::class, 'getSubCategories'])->name('product.get-subcategories');
+Route::get('product/get-child-categories', [VendorProductController::class, 'getChildCategories'])->name('product.get-child-categories');
+Route::put('product/change-status', [VendorProductController::class, 'changeStatus'])->name('product.change-status');
+Route::resource('products', VendorProductController::class);
