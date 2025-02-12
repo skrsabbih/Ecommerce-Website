2577a4e3befea0746959b47e31ca42e5619096e2
mahbub14 <mahbubhasan14@gmail.com>
2025-02-13 00:26:28 +0600

    v Products image gallery route updated

--- routes/vendor.php
+++ routes/vendor.php
@@ -3,6 +3,8 @@ use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Backend\vendorProfileController;
 use App\Http\Controllers\Backend\VendorShopProfileController;
 use App\Http\Controllers\Backend\VendorProductController;
+use App\Http\Controllers\Backend\VendorProductImageGalleryController;
+use App\Http\Controllers\Backend\VendorProductVariantController;
 use Illuminate\Support\Facades\Route;
 /** Vendor Routes **/
 Route::get('dashboard',[VendorController::class,'dashboard'])->name('dashboard');
@@ -17,3 +19,10 @@ Route::get('product/get-subcategories', [VendorProductController::class, 'getSub
 Route::get('product/get-child-categories', [VendorProductController::class, 'getChildCategories'])->name('product.get-child-categories');
 Route::put('product/change-status', [VendorProductController::class, 'changeStatus'])->name('product.change-status');
 Route::resource('products', VendorProductController::class);
+
+/** Products image gallery route */
+Route::resource('products-image-gallery', VendorProductImageGalleryController::class);
+
+/** Products variant route */
+Route::put('products-variant/change-status', [VendorProductVariantController::class, 'changeStatus'])->name('products-variant.change-status');
+Route::resource('products-variant', VendorProductVariantController::class);
